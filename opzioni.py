1112d60da1c62265950839aaa534d68b9e63c973
Francesco <cescolofaro@gmail.com>
2018-02-24 13:20:29 +0100

    Create opzioni.py

---
+++ opzioni.py
@@ -0,0 +1,27 @@
+# Credenziali ClasseViva #
+
+username = 'nome'
+password = 'password'
+
+# Token Discord per il bot #
+
+TokenDiscord = 'abc'
+
+# Opzioni #
+
+#Prefisso, carattere da inviare prima del comando
+Prefisso = '.'
+
+#impostare su "si" se vai a scuola il sabato
+sabato = 'no'
+
+# Ore #
+
+#Inserire il numero di ore per ogni giorno
+
+oreLun = 5
+oreMar = 5
+oreMer = 5
+oreGio = 5
+oreVen = 5
+oreSab = 5
